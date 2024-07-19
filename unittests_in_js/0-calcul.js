2d84d1af83fcbfecb1b3e1cbad1f475d2c1c66bc
Your Name <you@example.com>
2024-07-19 23:59:51 +0300

    corrected

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,6 @@
 // 0-calcul-1.js
 function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b);
-  }
-  
-  module.exports = calculateNumber;
-  
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
