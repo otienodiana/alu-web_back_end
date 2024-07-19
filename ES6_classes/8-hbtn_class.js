608f96cb55de1fe5d347176470b4a6e64b5fd5ac
Your Name <you@example.com>
2024-07-19 16:49:52 +0300

    corrected the ESlint error

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,12 +1,12 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;
-      this._location = location;
-    }
-  
-    [Symbol.toPrimitive](hint) {
-      if (hint === 'string') return this._location;
-      if (hint === 'number') return this._size;
-      return 42;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
   }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') return this._location;
+    if (hint === 'number') return this._size;
+    return 42;
+  }
+}
