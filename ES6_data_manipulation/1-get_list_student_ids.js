85572dad611f41ac4f5504b3d4aa23826f496478
Your Name <you@example.com>
2024-07-19 19:40:13 +0300

    correcting lint error

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
 export default function getListStudentIds(arrayOfObjects) {
-    const arr = [];
-    if (Array.isArray(arrayOfObjects)) {
-      return arrayOfObjects.map((item) => item.id);
-    }
-    return arr;
+  const arr = [];
+  if (Array.isArray(arrayOfObjects)) {
+    return arrayOfObjects.map((item) => item.id);
   }
+  return arr;
+}
+ 
