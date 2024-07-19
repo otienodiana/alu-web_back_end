ae4d738dbf7db3ef31f1ef16d878a989d58b8b2b
Your Name <you@example.com>
2024-07-20 00:07:20 +0300

    done

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,10 +6,7 @@ describe('calculateNumber', function () {
   it('should return 4 when inputs are 1 and 3', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-
-  it('should return 5 when inputs are 1 and 4', function () {
-    assert.strictEqual(calculateNumber(1, 4), 5);
-  });
+  
 
   it('should return 5 when inputs are 1.2 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
