1d8f18629c1a496128fb600e95b135bb94baa1c5
Your Name <you@example.com>
2024-07-19 15:57:48 +0300

    corrected the ESlint error

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,32 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set code(newCode) {
-      this._code = newCode;
-    }
-  
-    set name(newName) {
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string' || newCode.trim() === '') {
+      throw new TypeError('code must be a non-empty string');
+    }
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string' || newName.trim() === '') {
+      throw new TypeError('name must be a non-empty string');
+    }
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
