6cbf3230df0d1e4a21880ee8dd94f09ab6a2f480
Your Name <you@example.com>
2024-07-19 16:15:53 +0300

    correcting eslint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,20 @@
 export default class Building {
-    constructor(sqft) {
-      if (
-        this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function'
-      ) {
-        throw Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
-  
-      this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
     }
-  
-    get sqft() {
-      return this._sqft;
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+  
+  // Abstract method placeholder
+  evacuationWarningMessage() {
+    throw new Error('Method "evacuationWarningMessage" must be implemented');
+  }
+}
