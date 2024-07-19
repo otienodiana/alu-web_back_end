eb1c7c133ff0994d30430d7f365f234838e1ea9f
Your Name <you@example.com>
2024-07-19 16:20:18 +0300

    correcting errors

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,32 +1,18 @@
-import Building from './path-to-building';
-
-describe('Building Class', () => {
-  test('should throw an error if instantiated directly', () => {
-    expect(() => {
-      new Building(1000);
-    }).toThrow('Cannot instantiate abstract class Building directly');
-  });
-
-  test('should throw an error if subclass does not implement evacuationWarningMessage', () => {
-    class IncompleteBuilding extends Building {
-      // Missing evacuationWarningMessage implementation
-    }
-
-    expect(() => {
-      new IncompleteBuilding(1000);
-    }).toThrow('Class extending Building must override evacuationWarningMessage');
-  });
-
-  // Example of a valid subclass implementation
-  test('should not throw error if subclass implements evacuationWarningMessage', () => {
-    class CompleteBuilding extends Building {
-      evacuationWarningMessage() {
-        return 'Warning!';
+export default class Building {
+    constructor(sqft) {
+      if (
+        this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function'
+      ) {
+        throw Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
+  
+      this._sqft = sqft;
     }
-
-    expect(() => {
-      new CompleteBuilding(1000);
-    }).not.toThrow();
-  });
-});
+  
+    get sqft() {
+      return this._sqft;
+    }
+  }
