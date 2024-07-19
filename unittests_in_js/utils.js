52f3c64eae84d613be470e1a09cf52904e6ca629
Your Name <you@example.com>
2024-07-20 00:40:29 +0300

    update the utils file

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+class Utils {
+    static calculateNumber (type, a, b) {
+      if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+      } else if (type === 'DIVIDE') {
+        if (Math.round(b) === 0) {
+          return 'Error';
+        } else { return Math.round(a) / Math.round(b); }
+      } else if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      return 0;
+    }
+  }
+  
+  module.exports = Utils;
+  
