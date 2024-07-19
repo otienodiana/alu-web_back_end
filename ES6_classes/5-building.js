0c43acb04ed8aa0d2574174e3437776570dd39f6
Your Name <you@example.com>
2024-07-19 16:35:52 +0300

    done

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
     if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
+        'Class extending Building must override evacuationWarningMessage'
       );
     }
 
