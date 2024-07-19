ca893581b100be04db407f4070c4f7031b757c61
Your Name <you@example.com>
2024-07-19 23:01:16 +0300

    correcting the errors

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,25 +1,17 @@
-// Import the readline module to handle input and output
-const readline = require('readline');
-
-// Create an interface to read from stdin and write to stdout
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Display the welcome message
 console.log('Welcome to Holberton School, what is your name?');
 
-// Listen for the 'line' event which is triggered when the user inputs a line
-rl.on('line', (input) => {
-  // Display the user's name
-  console.log(`Your name is: ${input}`);
-  
-  // Close the readline interface and display the closing message
-  rl.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
 
-// Listen for the 'close' event which is triggered when the readline interface is closed
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
