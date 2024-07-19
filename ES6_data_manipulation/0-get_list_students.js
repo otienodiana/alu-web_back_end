fc1c6e30fceb284d1286e0aaccb589d5f868ba6e
Your Name <you@example.com>
2024-07-19 18:07:39 +0300

    solved indentation errors

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ export default function getListStudents() {
     return [
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
       { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' }, // Added trailing comma
+      { id: 5, firstName: 'Serena', location: 'San Francisco' }
     ];
   }
   
